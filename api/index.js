8aed8dba280ab4df1bbc61db6c96808ba60d9d2a
sramansamanta5 <sramansamanta2023@gmail.com>
2024-11-13 16:28:20 +0530

    Set up database connection and env variables

--- api/index.js
+++ api/index.js
@@ -1,6 +1,15 @@
 import express from 'express'
+import mongoose from 'mongoose';
+import dotenv from 'dotenv'
 
+
+
+dotenv.config();
 const app=express();
+
+mongoose.connect(process.env.MONGO_URI)
+.then(()=>{console.log("database is connected.")})
+.catch((err)=>{console.log(err)})
 
 app.listen(4000,()=>{
     console.log('App is up and running...')
