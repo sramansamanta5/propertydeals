091fc2205c9c2266bbd76ca1f38aefda4971cbd0
sramansamanta5 <sramansamanta2023@gmail.com>
2024-11-14 14:12:28 +0530

    Created usermodel,controller and routes folder

--- api/index.js
+++ api/index.js
@@ -1,6 +1,7 @@
 import express from 'express'
 import mongoose from 'mongoose';
 import dotenv from 'dotenv'
+import userRouter from './routes/userRoutes.js'
 
 
 
@@ -10,6 +11,12 @@ const app=express();
 mongoose.connect(process.env.MONGO_URI)
 .then(()=>{console.log("database is connected.")})
 .catch((err)=>{console.log(err)})
+
+app.use('/api/user',userRouter)
+
+app.get('/',(req,res)=>{
+    res.send("Home page")
+})
 
 app.listen(4000,()=>{
     console.log('App is up and running...')
