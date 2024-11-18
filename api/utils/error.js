52886f640dd4edd4bd04fb07f0728f40194bcc3c
sramansamanta5 <sramansamanta2023@gmail.com>
2024-11-18 15:44:28 +0530

    fixed a small mistake in error.js

--- api/utils/error.js
+++ api/utils/error.js
@@ -1,9 +1,9 @@
-export const errorHandler=(err,req,res,next)=>{
+export const errorHandler=(statuscode,message)=>{
 
     const error=new Error();
 
     error.statuscode=statuscode;
     error.message=message;
-    
+
     return error;
 }
